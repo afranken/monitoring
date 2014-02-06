044ff59f75b1152573d560658984c87886e2f15c
Arne Franken <arne.franken@coremedia.com>
2014-02-06 23:59:52 +0100

    formatting

--- src/main/javascript/init.js
+++ src/main/javascript/init.js
@@ -2,19 +2,19 @@
 requirejs.config({
   paths: {
     'Application': './Application',
-    'jquery': "./vendor/jquery-2.0.3",
-    'knockout': "./vendor/knockout-3.0.0"
+    'jquery': './vendor/jquery-2.0.3',
+    'knockout': './vendor/knockout-3.0.0'
   }
 });
 
 //initialize application
-require(["Application", "knockout"], function(ApplicationViewModel, ko) {
+require(['Application', 'knockout'], function(ApplicationViewModel, ko) {
   var applicationViewModel = new ApplicationViewModel(json);
   ko.applyBindings(applicationViewModel);
 });
 
 //
-require(["jquery"], function(jQuery){
+require(['jquery'], function(jQuery){
   jQuery(function(){
 
   });
