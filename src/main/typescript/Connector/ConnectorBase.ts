86786783d53b23fd1be533b8df115e0814eaf0da
Arne Franken <arne.franken@coremedia.com>
2014-02-27 15:01:58 +0100

    add documentation

--- src/main/typescript/Connector/ConnectorBase.ts
+++ src/main/typescript/Connector/ConnectorBase.ts
@@ -2,6 +2,9 @@ import Connector = require('./Connector');
 import Configuration = require('../Configuration/Configuration');
 import MonitorModel = require('../MonitorModel');
 
+/**
+ * Basic, abstract {@link Connector} aggregating methods used by all {@link Connector} implementations.
+ */
 class ConnectorBase implements Connector {
 
     private _configuration: Configuration;
@@ -10,6 +13,15 @@ class ConnectorBase implements Connector {
         this._configuration = configuration;
     }
 
+    /**
+     * Get an absolute URL for the given hostname, including configured
+     * {@link Config.Configuration.hosts.protocol}, {@link Config.Configuration.hosts.port},
+     * and {@link Config.Configuration.hosts.prefix}.
+     *
+     * @param hostname the hostname to get the URL for
+     * @param suffix optional suffix to be added to the URL
+     * @returns string the URL
+     */
     public getUrl(hostname:string, suffix?:string):string {
         var url:string = '';
         url = this._configuration.getProtocol(hostname);
@@ -23,10 +35,16 @@ class ConnectorBase implements Connector {
         return url;
     }
 
+    /**
+     * @returns number the configured {@link Config.Configuration.expiry}
+     */
     public getExpiry():number {
         return this._configuration.getExpiry();
     }
 
+    /**
+     * Not implemented, must be overwritten
+     */
     public getRemoteData(model:MonitorModel):void {
         throw {
             name: 'Error',
