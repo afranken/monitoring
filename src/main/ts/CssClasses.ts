e9bf2eef39e1bb403e2a0421d8cc81cc6ed6f77e
Arne Franken <arne.franken@coremedia.com>
2014-03-02 18:51:30 +0100

    better API documentation

--- src/main/ts/CssClasses.ts
+++ src/main/ts/CssClasses.ts
@@ -1,8 +1,16 @@
-
+/**
+ * This class contains CSS classes compatible with {@link http://getbootstrap.com/}.
+ */
 class CssClasses {
 
+    /**
+     * Many elements are displayed as alerts.
+     */
     public static BASIC: string = ' alert ';
 
+    /**
+     * Basic classes used by {@link JenkinsMonitorModel} and {@link NagiosMonitorModel}
+     */
     public static BASIC_CLASSES: string = ' jobstatus ' + CssClasses.BASIC;
 
     public static SUCCESS: string = ' alert-success ';
@@ -10,6 +18,10 @@ class CssClasses {
     public static FAILURE: string = ' alert-danger ';
     public static DISABLED: string = ' alert-disabled ';
 
+    /**
+     * If a {@link JenkinsMonitorModel} points to a Jenknis job that is still running, this class is used.
+     * The UI element will start to pulsate. (see {@link Application#registerPulsateBindingHandler}
+     */
     public static BUILDING: string = ' status-building ';
 
 }
