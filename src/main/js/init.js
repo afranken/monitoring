4e96041282153cbe6042a17c20e30a2e1be61c08
Arne Franken <arne.franken@coremedia.com>
2014-03-08 01:45:21 +0100

    render animations with less frames/second in oder to reduce CPU usage.

--- src/main/js/init.js
+++ src/main/js/init.js
@@ -1,5 +1,6 @@
 //initialize application
-require(['Application', 'knockout'], function(ApplicationViewModel, ko) {
+require(['Application', 'knockout', 'jquery'], function(ApplicationViewModel, ko) {
   var applicationViewModel = new ApplicationViewModel(json);
   ko.applyBindings(applicationViewModel);
+  jQuery.fx.interval = 40;
 });
