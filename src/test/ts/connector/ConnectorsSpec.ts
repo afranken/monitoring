0adf1e9a9c90d5ae5a9b4424bec64a8fcc1026d7
Arne Franken <arne.franken@coremedia.com>
2014-03-28 23:24:38 +0100

    add tests for Connectors

---
+++ src/test/ts/connector/ConnectorsSpec.ts
@@ -0,0 +1,29 @@
+/// <reference path="../jasmine"/>
+import Configuration = require('../../../main/ts/configuration/Configuration');
+import Connectors = require('../../../main/ts/connector/Connectors');
+import Connector = require('../../../main/ts/connector/Connector');
+import JenkinsConnector = require('../../../main/ts/jenkins/JenkinsConnector');
+import SonarConnector = require('../../../main/ts/sonar/SonarConnector');
+import NagiosConnector = require('../../../main/ts/nagios/NagiosConnector');
+
+/**
+ * Tests {@link Configuration}
+ */
+describe("Connectors", function():void {
+
+    var configuration:Configuration = new Configuration({});
+
+    var jenkinsConnector:Connector = Connectors.createConnector(configuration,"jenkins");
+    var nagiosConnector:Connector = Connectors.createConnector(configuration,"nagios");
+    var sonarConnector:Connector = Connectors.createConnector(configuration,"sonar");
+
+    /**
+     * Test all methods
+     */
+    it("TestMethods", function():void {
+        expect(jenkinsConnector instanceof JenkinsConnector).toBeTruthy();
+        expect(sonarConnector instanceof SonarConnector).toBeTruthy();
+        expect(nagiosConnector instanceof NagiosConnector).toBeTruthy();
+    });
+
+});
