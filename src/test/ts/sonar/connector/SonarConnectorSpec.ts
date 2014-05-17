dfdbd26632f87598bf3712cefefcfb972f12bec9
Arne Franken <arne.franken@coremedia.com>
2014-05-17 23:23:57 +0200

    use dataprovider in test

--- src/test/ts/sonar/connector/SonarConnectorSpec.ts
+++ src/test/ts/sonar/connector/SonarConnectorSpec.ts
@@ -6,54 +6,23 @@ import SonarConnector = require('../../../../main/ts/sonar/connector/SonarConnec
 import SonarJsonResponse = require('../../../../main/ts/jsonInterfaces/SonarResponse');
 import Config = require('../../../../main/ts/jsonInterfaces/Config');
 import Types = require('../../../../main/ts/util/Types');
+import SonarSpecDataProvider = require('../SonarSpecDataProvider');
 
 /**
  * Tests {@link SonarConnector}
  */
 describe('SonarConnector', function():void {
+    var dataProvider: SonarSpecDataProvider = new SonarSpecDataProvider();
 
-    var _HOST: string = 'myhost';
-    var _NAME: string = 'mymonitor';
-    var _EXPIRY: number = 888;
+    var monitor: SonarMonitorModel = <SonarMonitorModel>dataProvider.getSonarMonitorModel();
 
-    var configuration: Configuration = new Configuration({
-        hosts: [
-            {
-                hostname: _HOST,
-                protocol: 'https',
-                port: '8080',
-                prefix: 'myprefix'
-            }
-        ],
-        expiry: _EXPIRY
-    });
-
-
-    var monitorJson: Config.ExtendedMonitor = {
-        name: _NAME,
-        externalRef: [
-            {
-                name: 'myname',
-                id: 'myid'
-            },
-            {
-                name: 'myname2',
-                id: 'myid2'
-            }
-        ],
-        hostname: _HOST,
-        type: Types.SONAR
-    };
-
-    var monitor: SonarMonitorModel = <SonarMonitorModel>MonitorModels.createModel(monitorJson, configuration, _HOST);
-
-    var testling: SonarConnector = new SonarConnector(configuration);
+    var testling: SonarConnector = dataProvider.getSonarConnector();
 
     /**
      * Test all methods
      */
     it('TestMethods', function(): void {
-        expect(testling.getExpiry()).toBe(_EXPIRY);
+        expect(testling.getExpiry()).toBe(SonarSpecDataProvider.EXPIRY);
         expect(testling.getApiUrl(monitor,'mymodule'))
             .toBe('https://myhost:8080/myprefix/api/resources?callback=?&format=json&metrics=blocker_violations,critical_violations,major_violations,minor_violations,info_violations&resource=mymodule');
         expect(testling.getModuleUrl(monitor,'mymodule')).toBe('https://myhost:8080/myprefix/drilldown/violations/mymodule');
