07dc23926a0eda5d65fed59dc54fd2137e45e83d
Arne Franken <arne.franken@coremedia.com>
2014-07-27 21:24:40 +0200

    tslint cleanup

--- src/test/ts/SectionViewModelSpec.ts
+++ src/test/ts/SectionViewModelSpec.ts
@@ -18,64 +18,64 @@ import Config = require('Config');
 /**
  * Tests {@link SectionViewModel}
  */
-describe("SectionViewModel", function():void {
+describe('SectionViewModel', function(): void {
 
     var sectionJson1: Config.Section = {
-        "sections": [
+        sections: [
             {
-                "title": "1"
+                title: '1'
             }
         ]
     };
 
     var sectionJson3: Config.Section = {
-        "sections": [
+        sections: [
             {
-                "title": "1"
+                title: '1'
             },
             {
-                "title": "2"
+                title: '2'
             },
             {
-                "title": "3"
+                title: '3'
             }
         ]
     };
 
     var sectionJson7: Config.Section = {
-        "sections": [
+        sections: [
             {
-                "title": "1"
+                title: '1'
             },
             {
-                "title": "2"
+                title: '2'
             },
             {
-                "title": "3"
+                title: '3'
             },
             {
-                "title": "4"
+                title: '4'
             },
             {
-                "title": "5"
+                title: '5'
             },
             {
-                "title": "6"
+                title: '6'
             },
             {
-                "title": "7"
+                title: '7'
             }
         ]
     };
 
-    var testling1:SectionViewModel = SectionModels.createViewModel(sectionJson1,undefined,"");
-    var testling2:SectionViewModel = SectionModels.createViewModel(sectionJson3,undefined,"");
-    var testling3:SectionViewModel = SectionModels.createViewModel(sectionJson7,undefined,"");
+    var testling1: SectionViewModel = SectionModels.createViewModel(sectionJson1, undefined, '');
+    var testling2: SectionViewModel = SectionModels.createViewModel(sectionJson3, undefined, '');
+    var testling3: SectionViewModel = SectionModels.createViewModel(sectionJson7, undefined, '');
 
     /**
      * Test all methods
      */
-    it("TestMethods", function():void {
+    it('TestMethods', function(): void {
         expect(testling1.getColumnWidth()).toBe(12);
         expect(testling2.getColumnWidth()).toBe(4);
         expect(testling3.getColumnWidth()).toBe(1);
